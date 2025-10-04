a6dc769bae797559771dfb82fa31ac80cf9254fb
binary-shadow <web.dev.innovation.ru@gmail.com>
2025-10-04 03:14:09 +0300

    bundle minify for production | update README.md

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   dts: true,
-  sourcemap: true,
+  sourcemap: false, // Disable source maps for smaller bundle
   clean: true,
   format: ['esm', 'cjs'],
   target: 'es2020',
   outDir: 'dist',
   treeshake: true,
-  minify: false,
+  minify: true, // Minify for production
 });
