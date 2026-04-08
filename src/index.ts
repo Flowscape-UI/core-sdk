3fcdb27a808ad237c0238b8eb777d1795ee822ee
Nice Arti <nice_arti@mail.ru>
2026-04-08 21:24:34 +0300

    feat: add core and scene module exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./core";
+export * from "./grid";
+export * from "./input";
+export * from "./nodes";
+export * from "./renderer";
+export * from "./scene";
---
+++ src/scene/index.ts
@@ -0,0 +1,7 @@
+export * from "./InputManager";
+export * from "./LayerManager";
+export * from "./ManagerRenderHost";
+export * from "./RenderScheduler";
+export * from "./Scene";
+export * from "./types";
+export * from "./layers";
