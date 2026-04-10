f30989448ef2ff833f40ff0b3af070f7a2740b03
Nice Arti <nice_arti@mail.ru>
2026-04-10 18:14:59 +0300

    temp: comment eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,38 +6,38 @@ export default [
   {
     ignores: ['dist', 'node_modules', 'playground/', 'storybook-static/'],
   },
-  {
-    files: ['src/**/*.{ts,tsx}'],
-    languageOptions: {
-      sourceType: 'module',
-      parser: tsparser,
-      parserOptions: {
-        ecmaVersion: 2022,
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tseslint,
-      import: (await import('eslint-plugin-import')).default,
-    },
-    rules: {
-      ...eslint.configs.recommended.rules,
-      ...tseslint.configs['strict-type-checked'].rules,
-      ...tseslint.configs['stylistic-type-checked'].rules,
+  // {
+  //   files: ['src/**/*.{ts,tsx}'],
+  //   languageOptions: {
+  //     sourceType: 'module',
+  //     parser: tsparser,
+  //     parserOptions: {
+  //       ecmaVersion: 2022,
+  //       projectService: true,
+  //       tsconfigRootDir: import.meta.dirname,
+  //     },
+  //   },
+  //   plugins: {
+  //     '@typescript-eslint': tseslint,
+  //     import: (await import('eslint-plugin-import')).default,
+  //   },
+  //   rules: {
+  //     ...eslint.configs.recommended.rules,
+  //     ...tseslint.configs['strict-type-checked'].rules,
+  //     ...tseslint.configs['stylistic-type-checked'].rules,
 
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-      'import/order': ['warn', { 'newlines-between': 'always' }],
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-      ],
-      '@typescript-eslint/no-explicit-any': ['error'],
-    },
-    settings: {
-      'import/resolver': {
-        typescript: true,
-      },
-    },
-  },
+  //     'no-console': ['warn', { allow: ['warn', 'error'] }],
+  //     'import/order': ['warn', { 'newlines-between': 'always' }],
+  //     '@typescript-eslint/no-unused-vars': [
+  //       'warn',
+  //       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+  //     ],
+  //     '@typescript-eslint/no-explicit-any': ['error'],
+  //   },
+  //   settings: {
+  //     'import/resolver': {
+  //       typescript: true,
+  //     },
+  //   },
+  // },
 ];
